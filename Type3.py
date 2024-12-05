fdd9b0470d4d67b1a7057e1d9d6faa6828577335
Dishanta Sarma <147008603+Dish-Patched@users.noreply.github.com>
2024-12-04 19:20:43 -0600

    Update Type3.py

--- Type3.py
+++ Type3.py
@@ -29,8 +29,8 @@ def bot(username, password, url):
 
     WebDriverWait(driver, 5).until(ec.presence_of_element_located((By.ID, "dashboard")))
 
-username = "dishanta.sarma4@gmail.com"
-password = "Hagupuku12"
+username = ""
+password = ""
 
 url = "https://www.amazon.com/ap/signin?openid.pape.max_auth_age=900&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fpath%3D%252Fgp%252Fyourstore%252Fhome%26signIn%3D1%26useRedirectOnSuccess%3D1%26action%3Dsign-out%26ref_%3Dnav_AccountFlyout_signout&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0"
 
